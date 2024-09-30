b7712a66733a016674bad4b6d9eb86ab271ad7aa
isavega <ivega4@cuc.edu.co>
2024-09-29 19:50:30 -0500

    primera version

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miImagen from "./assets/Logo isa (1).png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="Logo pag" />
+        <h1>Hola Mundo!!!!</h1>
+        <p>Este es el taller #1 del 2do corte...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
